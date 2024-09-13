354b8a0b3afc570e12203ea311842c902dc6b33d
omardr777 <omarcoursee@gmail.com>
2024-09-13 23:29:09 +0300

    typing animated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,12 @@
 import { styles } from "../style";
-import { ComputerCanvas } from "./ComputerCanvas";
 import { motion } from "framer-motion";
+import { ShootingStars } from "./Meteors";
+import { TypeAnimation } from "react-type-animation";
 
 export const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <div className="relative w-full h-screen overflow-hidden">
+      {/* <section className="relative w-full h-screen mx-auto overflow-hidden"> */}
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
@@ -14,30 +16,65 @@ export const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText}`}>
-            hi I'm <span className="text-[#915eff]">Omar</span>
+            hi I'm <span className="text-[#915eff]">Omar</span>{" "}
+            <motion.span
+              animate={{ rotate: [0, 20, -20, 20, -20, 0] }}
+              transition={{
+                duration: 1.2,
+                ease: "easeInOut",
+                repeat: Infinity,
+                repeatDelay: 2,
+              }}
+              style={{
+                display: "inline-block",
+                transformOrigin: "bottom center",
+              }}
+            >
+              👋
+            </motion.span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Software engineer
-          </p>
+
+          <TypeAnimation
+            sequence={[
+              "Enthusiastic software engineer eager to learn,", // Types 'One'
+              0, // Waits 1s
+              "Enthusiastic software engineer eager to learn,\nexplore new technologies, and unravel", // Deletes 'One' and types 'Two'
+              0, // Waits 2s
+              "Enthusiastic software engineer eager to learn,\nexplore new technologies, and unravel\nthe mysteries of black boxes.", // Types 'Three' without deleting 'Two'
+            ]}
+            className={`${styles.heroSubText}`}
+            wrapper="span"
+            cursor={true}
+            speed={80}
+            style={{
+              whiteSpace: "pre-line",
+              display: "inline-block",
+              marginTop: "0.5rem",
+              color: "#f3f3f3",
+            }}
+          />
         </div>
       </div>
-      <ComputerCanvas />
-
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-        <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-center p-2">
-            <motion.div
-              animate={{ y: [0, 24, 0] }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className="size-3 rounded-full bg-secondary mb-1"
-            />
-          </div>
-        </a>
+      {/* <ComputerCanvas /> */}
+      <ShootingStars />
+      <div className="max-w-7xl max-h-7xl relative">
+        <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+          <a href="#about">
+            <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-center p-2">
+              <motion.div
+                animate={{ y: [0, 24, 0] }}
+                transition={{
+                  duration: 1.5,
+                  repeat: Infinity,
+                  repeatType: "loop",
+                }}
+                className="size-3 rounded-full bg-secondary mb-1"
+              />
+            </div>
+          </a>
+        </div>
       </div>
-    </section>
+    </div>
+    // </section>
   );
 };
