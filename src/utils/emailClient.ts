8fe85351f8885e99224c164dc1809c7672a0a1df
omardr777 <omarcoursee@gmail.com>
2024-09-13 17:33:01 +0300

    env & vecel

--- src/utils/emailClient.ts
+++ src/utils/emailClient.ts
@@ -7,16 +7,16 @@ export const EmailClient = async (
 ) => {
   try {
     emailJs.send(
-      import.meta.env.SERVICE_ID,
-      import.meta.env.TEMPLATE_ID,
+      import.meta.env.VITE_SERVICE_ID,
+      import.meta.env.VITE_TEMPLATE_ID,
       {
         from_name: name,
-        to_name: import.meta.env.TO_NAME,
+        to_name: import.meta.env.VITE_TO_NAME,
         from_email: email,
-        to_email: import.meta.env.TO_EMAIL,
+        to_email: import.meta.env.VITE_TO_EMAIL,
         message,
       },
-      import.meta.env.PUBLIC_KEY
+      import.meta.env.VITE_PUBLIC_KEY
     );
 
     onSuccess();
