3376f9f6d35ce2167ca423f12c5c8bf9ee62c13f
omardr777 <omarcoursee@gmail.com>
2024-09-13 13:05:33 +0300

    ref header

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,17 +1,14 @@
 import { motion } from "framer-motion";
-import { styles } from "../style";
-import { fadeIn, textVariant } from "../utils/motion";
+import { fadeIn } from "../utils/motion";
 import { services } from "../constants";
 import { ServiceCard } from "./ServiceCard";
 import { withSectionWrapper } from "../hoc/SectionWrapper";
+import { Header } from "./Header";
 
 const AboutC = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
-      </motion.div>
+      <Header text="Overview." subText="introduction" />
       <motion.p
         variants={fadeIn("right", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,19 +1,14 @@
 import { VerticalTimeline } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import { motion } from "framer-motion";
 import { experiences } from "../../constants";
 import { ExperienceCard } from "./ExperienceCard";
-import { styles } from "../../style";
-import { textVariant } from "../../utils/motion";
 import { withSectionWrapper } from "../../hoc/SectionWrapper";
+import { Header } from "../Header";
 
 const ExperienceC = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
-      </motion.div>
+      <Header text="Overview." subText="introduction" />
       <div className="mt-20 flex flex-col">
         <VerticalTimeline>
           {experiences.map((exp, i) => {
