6afd373dae6b297b505c4660575dec502958bec6
omardr777 <omarcoursee@gmail.com>
2024-09-14 01:58:28 +0300

    add assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -6,7 +6,9 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
+import sawi from "./sawi-icon.png";
+import axenda from "./axenda-icon.svg";
+import kaust from "./kaust-icon.jpg";
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
@@ -20,15 +22,20 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-
+import headerBanner from "./header-img.svg";
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
-
+import mostaqbali from "./mostaqbali.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+import chartWise from "./chartwise.png";
+import msd from "./msd.png";
+import pathfinding from "./pathfinding.png";
+import sortvis from "./sortvis.png";
+import blockchain from "./blockchain.png";
 
 export {
   logo,
@@ -59,4 +66,14 @@ export {
   carrent,
   jobit,
   tripguide,
+  axenda,
+  sawi,
+  kaust,
+  headerBanner,
+  mostaqbali,
+  chartWise,
+  msd,
+  pathfinding,
+  sortvis,
+  blockchain,
 };
