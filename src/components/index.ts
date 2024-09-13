0c15d6d66e5da508852fea991b294184d2c012ca
omardr777 <omarcoursee@gmail.com>
2024-09-13 17:48:39 +0300

    fix building using pnpm

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ export { StarCanvas } from "./StarsCanvas";
 export { About } from "./About";
 export { Contact } from "./Contact";
 export * from "./Experience/";
-export { Feedbacks } from "./Feedbacks";
 export { Hero } from "./Hero";
 export { Navbar } from "./Navbar";
 export { Tech } from "./Tech";
