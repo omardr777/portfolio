fe5cb44cbfab49fbc6a3a51539ff3bcd06f5e5dd
omardr777 <omarcoursee@gmail.com>
2024-09-17 20:50:13 +0300

    hearer animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,18 @@ interface IHeader {
   subText: string;
   text: string;
   id?: string;
+  displayAnimation?: boolean;
 }
-export const Header = ({ subText, text, id }: IHeader) => {
-  return (
-    <motion.div variants={textVariant()} id={id}>
+export const Header = ({ subText, text, id, displayAnimation }: IHeader) => {
+  return displayAnimation ? (
+    <motion.div variants={textVariant(0.1)} id={id}>
       <p className={styles.sectionSubText}>{subText}</p>
       <h2 className={styles.sectionHeadText}>{text}</h2>
     </motion.div>
+  ) : (
+    <div>
+      <p className={styles.sectionSubText}>{subText}</p>
+      <h2 className={styles.sectionHeadText}>{text}</h2>
+    </div>
   );
 };
